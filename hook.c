803a8f092f19a852aac232a4efaa8d8f8591a6b9
matcardo <matcardo@student.42sp.org.br>
2022-07-08 21:04:38 +0200

    all hooks works

--- hook.c
+++ hook.c
@@ -5,6 +5,14 @@ int handle_input(int keysys, t_win *win)
 {
 	if (keysys == 65307)
         close_window(win);
+	else if (keysys == 65361)
+        write(1, "lf\n", 3);
+	else if (keysys == 65362)
+        write(1, "up\n", 3);
+	else if (keysys == 65363)
+        write(1, "rg\n", 3);
+	else if (keysys == 65364)
+        write(1, "dw\n", 3);
 	return (0);
 }
 
